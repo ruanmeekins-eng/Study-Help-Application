288d756e13c0a7e06c379a450663ba1e580b0746
ruanmeekins-eng <ruanmeekins@gmail.com>
2026-04-16 17:36:24 -0400

    Updated password text field

--- src/main/java/com/example/studyhelpapp/LoginScreenController.java
+++ src/main/java/com/example/studyhelpapp/LoginScreenController.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -21,7 +22,7 @@ public class LoginScreenController {
     private TextField usernameTextField;
 
     @FXML
-    private TextField passwordTextField;
+    private PasswordField passwordTextField;
 
     @FXML
     private Label accountNotFoundLabel;
