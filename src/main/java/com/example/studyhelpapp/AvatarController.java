ee9bcb3f09f93ec65b99d771479b143d2122cd8c
sherls21 <Sherlyo2006@gmail.com>
2026-04-24 18:43:49 -0400

    Added avatar shop with a background and buy feature + equip/unequip feature.

--- src/main/java/com/example/studyhelpapp/AvatarController.java
+++ src/main/java/com/example/studyhelpapp/AvatarController.java
@@ -3,17 +3,34 @@ package com.example.studyhelpapp;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class AvatarController {
+
     @FXML
     private Button homeButton;
 
     @FXML
-    public void initialize(){
+    private Button buyButton;
+
+    @FXML
+    private ImageView BrownGirlHairEquip;
+
+    private boolean brownHairOwned = false;
+    private boolean brownHairEquipped = false;
+
+    @FXML
+    public void initialize() {
         setupEventHandlers();
+
+        BrownGirlHairEquip.setVisible(false);
+        buyButton.setText("Buy");
     }
 
     @FXML
@@ -28,6 +45,42 @@ public class AvatarController {
                 }
             }
         });
+    }
 
+    @FXML
+    public void handleBrownHairButton() {
+
+        if (!brownHairOwned) {
+            Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+            confirm.setTitle("Confirm Purchase");
+            confirm.setHeaderText("Buy Brown Hair?");
+            confirm.setContentText("Are you sure you want to buy this item?");
+
+            Optional<ButtonType> result = confirm.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                brownHairOwned = true;
+                brownHairEquipped = true;
+
+                BrownGirlHairEquip.setVisible(true);
+                buyButton.setText("Unequip");
+
+                Alert success = new Alert(Alert.AlertType.INFORMATION);
+                success.setHeaderText(null);
+                success.setContentText("Item purchased and equipped!");
+                success.showAndWait();
+            }
+
+        } else {
+            if (brownHairEquipped) {
+                brownHairEquipped = false;
+                BrownGirlHairEquip.setVisible(false);
+                buyButton.setText("Equip");
+            } else {
+                brownHairEquipped = true;
+                BrownGirlHairEquip.setVisible(true);
+                buyButton.setText("Unequip");
+            }
+        }
     }
 }
